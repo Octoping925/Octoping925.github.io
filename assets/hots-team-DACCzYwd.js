91fe63a2c1d8141f74ccdd5a8c7807532d16edca
문영채 <ycmoon@yna.co.kr>
2025-06-17 16:20:37 +0900

    Updates

--- assets/hots-team-DACCzYwd.js
+++ assets/hots-team-DACCzYwd.js
@@ -1,4 +1,4 @@
-import{j as a,r as i}from"./index-bV0IqbDu.js";const S=({children:t,id:r})=>{const o=s=>{s.dataTransfer.setData("text/plain",r)};return a.jsx("div",{draggable:!0,onDragStart:o,className:"cursor-move",children:t})},y=({children:t,onDrop:r,className:o=""})=>{const s=n=>{n.preventDefault()},l=n=>{n.preventDefault();const d=n.dataTransfer.getData("text/plain");r(d)};return a.jsx("div",{onDragOver:s,onDrop:l,className:o,children:t})};/**
+import{j as a,r as i}from"./index-C_7l9XoF.js";const S=({children:t,id:r})=>{const o=s=>{s.dataTransfer.setData("text/plain",r)};return a.jsx("div",{draggable:!0,onDragStart:o,className:"cursor-move",children:t})},y=({children:t,onDrop:r,className:o=""})=>{const s=n=>{n.preventDefault()},l=n=>{n.preventDefault();const d=n.dataTransfer.getData("text/plain");r(d)};return a.jsx("div",{onDragOver:s,onDrop:l,className:o,children:t})};/**
  * @license lucide-react v0.516.0 - ISC
  *
  * This source code is licensed under the ISC license.

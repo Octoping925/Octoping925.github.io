b8139ef9e73fc8d9a834ae6b59b6fe4951a48e73
문영채 <ycmoon@yna.co.kr>
2025-06-17 14:37:49 +0900

    Hots 팀 메이커 구현

---
+++ pages/hots-team/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HotsTeamMaker from "../../component/HotsTeamMaker";
+
+const HotsTeamPage: React.FC = () => {
+  return <HotsTeamMaker />;
+};
+
+export default HotsTeamPage;
